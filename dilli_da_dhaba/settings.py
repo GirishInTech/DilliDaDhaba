734f8721abfb33971d52f424fb3bc673d3eebfde
GirishInTech <girish4936@gmail.com>
2026-02-22 22:41:51 +0530

    Initial Django restaurant project

--- dilli_da_dhaba/settings.py
+++ dilli_da_dhaba/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, True),
-    ALLOWED_HOSTS=(list, ['localhost', '127.0.0.1']),
+    ALLOWED_HOSTS=(list, ['localhost', '127.0.0.1','https://dillidadhaba.onrender.com/']),
 )
 
 # Load .env only if it exists (not present on Render — uses env vars directly)
